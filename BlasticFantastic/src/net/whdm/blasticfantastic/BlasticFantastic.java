f47328d3b78100a6cb7ffb4025124e1bca0dafcc
Robin Jonsson <robin.jonsson@yh.nackademin.se>
2012-11-04 16:10:10 +0100

    Version 0.1-1a
    Added librarys (lwjgl, slick, slick-util) and lwjgl native 32bit
    (native32bitwindows)
    Also added test map, and made a background

--- BlasticFantastic/src/net/whdm/blasticfantastic/BlasticFantastic.java
+++ BlasticFantastic/src/net/whdm/blasticfantastic/BlasticFantastic.java
@@ -1,16 +1,21 @@
 package net.whdm.blasticfantastic;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.tiled.TiledMap;
  
 public class BlasticFantastic extends BasicGame{
  
     Image plane = null;
     Image land = null;
+    Rectangle viewport;
+    //TiledMap map1;
     int x = 100;
     int y = 100;
     int scale = 1;
@@ -21,62 +26,29 @@ public class BlasticFantastic extends BasicGame{
     }
  
     @Override
-    public void init(GameContainer gc)
-			throws SlickException {
+    public void init(GameContainer gc) throws SlickException {
         plane = new Image("data/plane.png");
         land = new Image("data/land.jpg");
+        viewport = new Rectangle(0, 0, 800, 600);
+        //map1 = new TiledMap("data/test.tmx");
     }
  
     @Override
-    public void update(GameContainer gc, int delta)
-			throws SlickException
-    {
-        Input input = gc.getInput();
- 
-        if(input.isKeyDown(Input.KEY_A))
-        {
-            plane.rotate(-0.2f * delta);
-        }
- 
-        if(input.isKeyDown(Input.KEY_D))
-        {
-            plane.rotate(0.2f * delta);
-        }
- 
-        if(input.isKeyDown(Input.KEY_W))
-        {
-            float hip = 0.4f * delta;
- 
-            float rotation = plane.getRotation();
- 
-            x+= hip * Math.sin(Math.toRadians(rotation));
-            y-= hip * Math.cos(Math.toRadians(rotation));
-        }
- 
-        if(input.isKeyDown(Input.KEY_2))
-        {
-            scale += (scale >= 5.0f) ? 0 : 0.1f;
-            plane.setCenterOfRotation(plane.getWidth()/2.0f*scale, plane.getHeight()/2.0f*scale);
-        }
-        if(input.isKeyDown(Input.KEY_1))
-        {
-            scale -= (scale <= 1.0f) ? 0 : 0.1f;
-            plane.setCenterOfRotation(plane.getWidth()/2.0f*scale, plane.getHeight()/2.0f*scale);
-        }
+    public void update(GameContainer gc, int delta) throws SlickException {
+    	
     }
  
-    public void render(GameContainer gc, Graphics g)
-			throws SlickException
-    {
-        land.draw(0, 0);
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+    	g.setColor(new Color(72, 152, 72));
+    	g.fillRect(0,0,4800,4800); //Background
+    	//map1.render(0,0);
+        //land.draw(0, 0);
  
-        plane.draw(x, y, scale);
+        //plane.draw(x, y, scale);
  
     }
  
-    public static void main(String[] args)
-			throws SlickException
-    {
+    public static void main(String[] args) throws SlickException {
          AppGameContainer app =
 			new AppGameContainer( new BlasticFantastic() );
          app.setVSync(true);
