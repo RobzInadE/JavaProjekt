c5dc5118225f981ab49b2c960e049f50cb0ad8d1
Robin Jonsson <robin.jonsson@yh.nackademin.se>
2012-11-08 11:54:09 +0100

    Large updated Added working gravity.

--- BlasticFantastic/src/net/whdm/blasticfantastic/BlasticFantastic.java
+++ BlasticFantastic/src/net/whdm/blasticfantastic/BlasticFantastic.java
@@ -1,4 +1,13 @@
 package net.whdm.blasticfantastic;
+import org.jbox2d.collision.shapes.PolygonShape;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyDef;
+import org.jbox2d.dynamics.BodyType;
+import org.jbox2d.dynamics.FixtureDef;
+import org.jbox2d.dynamics.World;
+import org.lwjgl.opengl.GL11;
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
@@ -10,15 +19,23 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.tiled.TiledMap;
  
-public class BlasticFantastic extends BasicGame{
+public class BlasticFantastic extends BasicGame {
  
     Image plane = null;
     Image land = null;
     Rectangle viewport;
-    //TiledMap map1;
+    Player runningMan1;
+    Animation runningMan1anim;
+    TiledMap map1;
     int x = 100;
     int y = 100;
     int scale = 1;
+    public int displayList;
+    public World world;
+    public float timeStep;
+    public int velocityIterations, positionIterations;
+    public Body body;
+    public Vec2 position;
  
     public BlasticFantastic()
     {
@@ -30,24 +47,90 @@ public class BlasticFantastic extends BasicGame{
         plane = new Image("data/plane.png");
         land = new Image("data/land.jpg");
         viewport = new Rectangle(0, 0, 800, 600);
-        //map1 = new TiledMap("data/test.tmx");
+        runningMan1 = new Player(5, 12.5f, 4, 12, 32, 40, "data/eri3.png", 50);
+        runningMan1.getAnimation("idle").start();
+        
+        map1 = new TiledMap("data/1.tmx");
+        
+        //Create our World
+        Vec2 gravity = new Vec2(0, 100);
+        boolean doSleep = true;
+        world = new World(gravity,doSleep);
+        
+        BodyDef groundBodyDef = new BodyDef();
+        groundBodyDef.position.set(5, 27);
+        Body groundBody = world.createBody(groundBodyDef);
+        PolygonShape groundBox = new PolygonShape();
+        groundBox.setAsBox(10, 1);
+        groundBody.createFixture(groundBox, 0);
+
+        // Dynamic Body
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyType.DYNAMIC;
+        bodyDef.position.set(runningMan1.x, runningMan1.y);
+        body = world.createBody(bodyDef);
+        PolygonShape dynamicBox = new PolygonShape();
+        dynamicBox.setAsBox(2, 2.5f);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = dynamicBox;
+        fixtureDef.density=1;
+        fixtureDef.friction=0.5f;
+        //fixtureDef.restitution=0.5f;
+        body.createFixture(fixtureDef);
+
+        // Setup world
+        timeStep = 1.0f/60.0f;
+        velocityIterations = 6;
+        positionIterations = 2;
+        
+        position = body.getPosition();
+
     }
  
     @Override
     public void update(GameContainer gc, int delta) throws SlickException {
-    	
+    	if (gc.getInput().isKeyDown(Input.KEY_RIGHT)) {
+    		runningMan1.getAnimation("right").start();
+    		body.setLinearVelocity(new Vec2(5, 0));
+    	}
+    	else if (gc.getInput().isKeyDown(Input.KEY_LEFT)) {
+    		runningMan1.getAnimation("left").start();
+    	}
+    	else  {
+    		runningMan1.getAnimation("idle").start();
+    	}
+    	world.step(timeStep, velocityIterations, positionIterations);
+    	position = body.getPosition();
+    	System.out.println(body.getLinearVelocity().y);
+        runningMan1.sX(position.x);
+        runningMan1.sY(position.y);
     }
  
     public void render(GameContainer gc, Graphics g) throws SlickException {
-    	g.setColor(new Color(72, 152, 72));
+    	g.setColor(new Color(60, 66, 86));
     	g.fillRect(0,0,4800,4800); //Background
-    	//map1.render(0,0);
-        //land.draw(0, 0);
- 
-        //plane.draw(x, y, scale);
+    	g.setClip(viewport);
+    	map1.render(0, 0);
+    	if(runningMan1.currentDirection()==0) {
+    		//Player is idle, animate idle.
+    		g.drawAnimation(runningMan1.getAnimation("idle"), runningMan1.x*16, runningMan1.y*16);
+    	}
+    	else if(runningMan1.currentDirection()==1) {
+    		//Player is running left, animate left.
+    		g.drawAnimation(runningMan1.getAnimation("left"), runningMan1.x*16, runningMan1.y*16);
+    	}
+    	else if(runningMan1.currentDirection()==3) {
+    		//Player is running right, animate right.
+    		g.drawAnimation(runningMan1.getAnimation("right"), runningMan1.x*16, runningMan1.y*16);
+    	}
+    	//System.out.println(world.getGravity().y);
+    	
+    	
+
  
     }
- 
+    
+    
     public static void main(String[] args) throws SlickException {
          AppGameContainer app =
 			new AppGameContainer( new BlasticFantastic() );
--- BlasticFantastic/src/net/whdm/blasticfantastic/Player.java
+++ BlasticFantastic/src/net/whdm/blasticfantastic/Player.java
@@ -1,45 +1,87 @@
 package net.whdm.blasticfantastic;
 
-import org.jbox2d.collision.shapes.CircleShape;
-import org.jbox2d.dynamics.Body;
-import org.jbox2d.dynamics.BodyType;
-import org.jbox2d.dynamics.FixtureDef;
-import org.jbox2d.dynamics.World;
-import org.newdawn.slick.Image;
-import org.newdawn.slick.SlickException;
-import org.jbox2d.dynamics.BodyDef;
 
-public class Player {
-	private float posX;
-	private float posY;
-	private String image;
+import org.jbox2d.dynamics.BodyDef;
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
+
+public class Player extends BodyDef{
+	public float x;
+	public float y;
+	private int idleTiles;
+	private int runningTiles;
+	private int tileWidth;
+	private int tileHeight;
+	private String spritesheet;
+	private SpriteSheet sprites;
+	private Animation player_running_left, player_running_right, player_idle;
+	private int delay;
+	private int direction;
 	
-	private BodyType bodyType;
-	
-	public Player(float posX, float posY, BodyType bodyType, String image) {
-		this.posX = posX;
-		this.posY = posY;
-		this.bodyType = bodyType;
-		this.image = image;
+	Player(float x, float y, int idleTiles, int runningTiles, int tileWidth, int tileHeight, String spritesheet, int delay) throws SlickException {
+		this.x = x;
+		this.y = y;
+		this.runningTiles = runningTiles;
+		this.idleTiles = idleTiles;
+		this.spritesheet = spritesheet;
+		this.tileWidth = tileWidth;
+		this.tileHeight = tileHeight;
+		this.delay = delay;
+		this.sprites = new SpriteSheet(this.spritesheet,this.tileWidth,this.tileHeight);
+		
+		this.player_idle = new Animation();
+		this.player_running_right = new Animation();
+		this.player_running_left = new Animation();
+		
+		//Create idle animation
+		for (int frame=0;frame<idleTiles;frame++) {
+			player_idle.addFrame(sprites.getSprite(frame,0), delay);
+		}
+		//Loop back and forth
+		player_idle.setPingPong(true);
+		
+		//Create running right
+		for (int frame=0;frame<runningTiles;frame++) {
+			player_running_right.addFrame(sprites.getSprite(frame,1), delay);
+		}
+		
+		//Create running left (Mirrored.)
+		for (int frame=runningTiles-1;frame>0;frame--) {
+			player_running_left.addFrame(sprites.getSprite(frame,2), delay);
+		}
+		
+	}
+	public Animation getAnimation(String state) {
+		if(state=="right") {
+			this.direction = 3; 
+			return this.player_running_right;
+		}
+		else if(state=="idle") {
+			this.direction = 0;
+			return this.player_idle;
+		}
+		else if(state=="left") {
+			this.direction = 1;
+			return this.player_running_left;
+		}
+		else {
+			this.direction = 0;
+			return new Animation();
+		}
+	}
+
+	public void sX(float xy) {
+		this.x=xy;
+	}
+	public void sY(float yx) {
+		this.y=yx;
 	}
 	
-	public Image create() throws SlickException {
-		Image player = new Image(this.image);
-		
-		BodyDef bd = new BodyDef();
-		bd.type = bodyType;
-		bd.position.set(posX, posY);
-		
-		FixtureDef fd = new FixtureDef();
-		fd.shape = new CircleShape();
-		fd.shape.m_radius = (player.getHeight() / 2);
-		fd.density = 1;
-		fd.friction = 0.3f;
-		fd.restitution = 0.6f;
-		
-		Body body = Utils.world.createBody(bd);
-		body.createFixture(fd);
-		
-		return player;
+	
+	public int currentDirection() {
+		return this.direction;
 	}
+	
+	
 }
